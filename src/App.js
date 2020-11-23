2d18339f8772b7f3c0b90751d7eb56f5a004aeae
Manoj Chennareddy <52932901+ManojChenna@users.noreply.github.com>
2020-11-23 23:19:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Accordion from "./Components/Accordion";
+
+const items = [
+  {
+    title: "Question 1?",
+    content: "Answer 1."
+  },
+  {
+    title: "Question 2?",
+    content: "Answer 2."
+  },
+  {
+    title: "Question 3?",
+    content: "Answer 3."
+  }
+];
+export default () => {
+  return (
+    <div className="App">
+      <br />
+      <Accordion items={items} />
+    </div>
+  );
+};
